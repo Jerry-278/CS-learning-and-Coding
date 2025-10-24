39726fbbe5eb604ef95949b1976ccca813fbfe7e
Yi Ming Wang <jerrywangym278@gmail.com>
2025-10-24 09:31:57 +0300

    Java program
    A Java program that computes the area of a regular hexagon based on user input.

---
+++ Hexagon.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Hexagon {
+    private int length; // field to store the side length
+
+    public Hexagon() {
+        input();
+    }
+
+    public void input() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the side length of the hexagon: ");
+        length = in.nextInt();
+        calculateArea();
+        in.close();
+    }
+
+    public void calculateArea() {
+        if (length <= 0) {
+            System.out.println("Side length cannot be 0 or smaller than 0.");
+        } else {
+            double area = (3 * Math.sqrt(3) / 2) * Math.pow(length, 2);
+            System.out.printf("The area of the hexagon is: %.2f%n", area);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Hexagon();
+    }
+}
